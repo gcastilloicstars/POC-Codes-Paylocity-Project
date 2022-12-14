4e6567f75cf38e3c9e6cf8e4d5e466e9ea54c48c
Giovanny Castillo <111474622+gcastilloicstars@users.noreply.github.com>
2022-12-14 13:03:29 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import ImpressionList from './impressionList';
+import axios from "axios";
+
+function App() {
+  var testList = [
+    {name:"a", description: "impressionA"}, 
+    {name:"b", description: "impressionB"}, 
+    {name:"c", description: "impressionC"}, 
+    {name:"d", description: "impressionD"}, 
+  ]
+  const realList = axios.get("localhost:4008/api/impressions");
+  
+  return (
+    <div className="App">
+     <ImpressionList name={"Gio"} impressions={testList}/> 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/impressionList.js
@@ -0,0 +1,16 @@
+
+const ImpressionList = ({name, impressions}) =>  {
+
+    return (
+        <div> 
+        <h2> This is the list for {name} </h2>
+        {impressions && impressions.map(i => {
+        return    <div>  {i.name} :{i.description} </div>;
+        }
+        
+        )} 
+        </div>
+    )
+}
+
+export default ImpressionList;
